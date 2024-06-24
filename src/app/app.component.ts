0cf97eb8f7cff60f0d3fc7fa5784660baa53b4df
RakhiPatil <rakhipatil2608@gmail.com>
2024-06-24 19:40:35 +0530

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,16 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ShopieService } from './shopie.service';
 import { HttpClientModule } from '@angular/common/http';
+import { CartComponent } from './cart/cart.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { ProfileComponent } from './profile/profile.component';
+import { RegisterComponent } from './register/register.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,HttpClientModule],
+  imports: [RouterOutlet,HttpClientModule,CartComponent, DashboardComponent, HomepageComponent, ProfileComponent,RegisterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { HomepageComponent } from './homepage/homepage.component';
+import { RegisterComponent } from './register/register.component';
+import { CartComponent } from './cart/cart.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProfileComponent } from './profile/profile.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {'path':'cart', component:CartComponent},
+    {'path':'dashboard', component:DashboardComponent},
+    {'path':'home', component:HomepageComponent},
+    {'path':'profile', component:ProfileComponent},
+    {'path': 'register', component: RegisterComponent },
+    {'path':'', component:HomepageComponent},
+  
+];
